d5e25b452d54e62494452351699fb1358fd5ed17
calvares <calvares@student.42lisboa.com>
2026-01-30 17:17:16 +0000

    first in python

---
+++ Python3/remove_duplicates.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicates(self, nums: list) -> int: #iterate the list and check if the last value is equal to the current one
+        if not nums:
+            return 0
+        
+        i = 1
+        rewrite = 1
+        while i < len(nums):
+            if nums[i] != nums[i - 1]:
+                nums[rewrite] = nums[i]
+                rewrite += 1
+            i += 1
+        return rewrite
+
+if __name__ == "__main__":
+    nums = [1,1,2]
+    sol = Solution()
+    k = sol.removeDuplicates(nums)
+    print(nums)
